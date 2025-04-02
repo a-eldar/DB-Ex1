7d8a6779e628b3b35c2a3a0090cb900d5d27f23e
ofekelly <ofek.kelly@mail.huji.ac.il>
2025-04-02 11:37:13 +0300

    change the order of the tabels

--- create.sql
+++ create.sql
@@ -6,48 +6,49 @@
 
 -- `year` might be datatype
 
-create table university(
-	iau_id1 varchar PRIMARY KEY,
-	foundedyr integer CHECK(foundedyr >= 0) NOT NULL,
-	yrclosed integer CHECK(yrclosed >= foundedyr),
-	eng_name varchar NOT NULL, 
-	orig_name varchar NOT NULL,
-	private01 boolean,
-	latitude float, 
-	longitude float,
-	phd_granting boolean,
-	divisions integer, 
-	specialized boolean,
-	countrycode char(3) NOT NULL,
-	FOREIGN KEY(countrycode) REFERENCES country(countrycode) ON DELETE CASCADE,
-);
-
 create table country(
 	country varchar UNIQUE,
 	countrycode char(3) PRIMARY KEY, 
-	region varchar CHECK(region in (
-		"East Asia and Pacific",
-	 	"Europe and Central Asia",
-		"Latin America and Caribbean",
-		"Middle East and North Africa",
-		"North America",
-		"South Asia",
-		"Sub-Saharan Africa"
-		)), 
-	incomegroup varchar CHECK(incomegroup in (
-		"Low income",
-		"Lower middle income",
-		"Upper middle income",
-		"High income"
-	)),
+	region VARCHAR CHECK (region IN (
+        'East Asia and Pacific',
+        'Europe and Central Asia',
+        'Latin America and Caribbean',
+        'Middle East and North Africa',
+        'North America',
+        'South Asia',
+        'Sub-Saharan Africa'
+    )), 
+	incomegroup VARCHAR CHECK (incomegroup IN (
+        'Low income',
+        'Lower middle income',
+        'Upper middle income',
+        'High income'
+    ))
+);
+
+
+CREATE TABLE university (
+    iau_id1 VARCHAR PRIMARY KEY,
+    foundedyr INTEGER CHECK(foundedyr >= 0) NOT NULL,
+    yrclosed INTEGER CHECK(yrclosed >= foundedyr),
+    eng_name VARCHAR NOT NULL, 
+    orig_name VARCHAR NOT NULL,
+    private01 BOOLEAN,
+    latitude FLOAT, 
+    longitude FLOAT,
+    phd_granting BOOLEAN,
+    divisions INTEGER, 
+    specialized BOOLEAN,
+    countrycode CHAR(3) NOT NULL,
+    FOREIGN KEY (countrycode) REFERENCES country(countrycode) ON DELETE CASCADE
 );
 
 
 
-create table students_accepted(
-	year integer CHECK(year >= 0),
-	iau_id1 varchar,
-	students5_estimated integer CHECK (students5_estimated >= 0),
-	PRIMARY KEY(iau_id1, year),
-	FOREIGN KEY(iau_id1) REFERENCES university(iau_id1) ON DELETE CASCADE
+CREATE TABLE students_accepted (
+    year INTEGER CHECK(year >= 0),
+    iau_id1 VARCHAR,
+    students5_estimated INTEGER CHECK (students5_estimated >= 0),
+    PRIMARY KEY (iau_id1, year),
+    FOREIGN KEY (iau_id1) REFERENCES university(iau_id1) ON DELETE CASCADE
 );
